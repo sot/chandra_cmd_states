a76d4a01a9a64f5a2b8f9014e479eb99349537e7
Tom Aldcroft <taldcroft@gmail.com>
2012-07-02 22:02:16 -0400

    Change update_cmd_states.py and get_cmd_states.py into wrapper scripts

--- get_cmd_states.py
+++ get_cmd_states.py
@@ -3,91 +3,25 @@
 Get the Chandra commanded states over a range of time as a space-delimited
 ASCII table.
 
+Usage: get_cmd_states.py [options]
+
+Options:
+  -h, --help           show this help message and exit
+  --start=START        Start date (default=Now-10 days)
+  --stop=STOP          Stop date (default=None)
+  --vals=VALS          Comma-separated list of state values.  Possible values
+                       are:     obsid power_cmd si_mode vid_board clocking
+                       fep_count ccd_count     simpos simfa_pos hetg letg
+                       pcad_mode pitch ra dec roll q1 q2 q3 q4     trans_keys
+                       dither
+  --allow-identical    Allow identical states from cmd_states table
+                       (default=False)
+  --outfile=OUTFILE    Output file (default=stdout)
+  --dbi=DBI            Database interface (default=sybase)
+  --server=SERVER      DBI server (default=sybase)
+  --user=USER          sybase database user (default='aca_read')
+  --database=DATABASE  sybase database (default=Ska.DBI default)
 """
-import Ska.DBI
-from Chandra.Time import DateTime
-import sys
+
 import Chandra.cmd_states
-import numpy
-import Ska.Numpy
-from numpy.lib import recfunctions
-
-STATE_VALS = """\
-obsid power_cmd si_mode vid_board clocking fep_count ccd_count
-simpos simfa_pos hetg letg
-pcad_mode pitch ra dec roll q1 q2 q3 q4
-trans_keys
-dither
-"""
-
-def get_options():
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.set_defaults()
-    parser.add_option("--start",
-                      help="Start date (default=Now-10 days)")
-    parser.add_option("--stop",
-                      help="Stop date (default=None)")
-    parser.add_option("--vals",
-                      help="Comma-separated list of state values.  "
-                      "Possible values are:\n" + STATE_VALS,)
-    parser.add_option("--allow-identical",
-                      default=False,
-                      action='store_true',
-                      help="Allow identical states from cmd_states table (default=False)")
-    parser.add_option("--outfile",
-                      help="Output file (default=stdout)")
-    parser.add_option("--dbi",
-                      default='sybase',
-                      help="Database interface (default=sybase)")
-    parser.add_option("--server",
-                      default='',
-                      help="DBI server (default=sybase)")
-    parser.add_option("--user",
-                      default='aca_read',
-                      help="sybase database user (default='aca_read')")
-    parser.add_option("--database",
-                      help="sybase database (default=Ska.DBI default)")
-
-
-    (opt, args) = parser.parse_args()
-    return (opt, args)
-
-def main():
-    opt, args = get_options()
-
-    allowed_state_vals = STATE_VALS.split()
-    if opt.vals is None:
-        state_vals = allowed_state_vals
-    else:
-        state_vals = opt.vals.split(',')
-        bad_state_vals = set(state_vals).difference(allowed_state_vals)
-        if bad_state_vals:
-            print ('ERROR: requested --values {0} are not allowed '
-                   '(see get_cmd_states.py --help)'.format(','.join(sorted(bad_state_vals))))
-            sys.exit(0)
-
-    try:
-        db = Ska.DBI.DBI(dbi=opt.dbi, server=opt.server, user=opt.user, database=opt.database, verbose=False)
-    except Exception, msg:
-        print 'ERROR: failed to connect to {0}:{1} server: {2}'.format(opt.dbi, opt.server, msg)
-        sys.exit(0)
-
-    start = DateTime(opt.start) if opt.start else DateTime() - 10
-    db_states_q = """SELECT * from cmd_states
-                     WHERE datestop > '%s'""" % (start.date)
-    if opt.stop:
-        db_states_q += " AND datestart < '%s'" % DateTime(opt.stop).date
-    db_states = db.fetchall(db_states_q)
-
-    if opt.vals:
-        db_states = Chandra.cmd_states.reduce_states(db_states, state_vals,
-                                                     allow_identical=opt.allow_identical)
-        drop_fields = set(allowed_state_vals) - set(state_vals)
-        db_states = recfunctions.rec_drop_fields(db_states, drop_fields)
-
-    out = open(opt.outfile, 'w') if opt.outfile else sys.stdout
-    out.write(Ska.Numpy.pformat(db_states))
-
-if __name__ == '__main__':
-    main()
+Chandra.cmd_states.get_cmd_states()
