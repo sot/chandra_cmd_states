7049f66cfc028a091ad1c6c92c300a042bdcaefe
Tom Aldcroft <taldcroft@gmail.com>
2012-07-06 10:38:01 -0400

    Add support for HDF5 version of cmd_states table.
    New code auto-generates the initial HDF5 clone of the SQL
    cmd_states table, then udpates the HDF5 version in lock step
    with SQL (sybase or sqlite).  Some consistency checks are applied
    to ensure the two tables have the same length and same final
    datestart.

--- Chandra/cmd_states.py
+++ Chandra/cmd_states.py
@@ -8,8 +8,11 @@ import re
 import logging
 import os
 import time
+import pprint
 
 import numpy as np
+import tables
+
 import Ska.File
 import Ska.DBI
 from Chandra.Time import DateTime
@@ -17,7 +20,6 @@ import Chandra.Maneuver
 from Quaternion import Quat
 import Ska.ParseCM
 import Ska.Numpy
-import pprint
 
 # Canonical state0 giving spacecraft state at beginning of timelines
 # 2002:007:13 fetch --start 2002:007:13:00:00 --stop 2002:007:13:02:00 aoattqt1
@@ -47,7 +49,7 @@ STATE0 = {'ccd_count': 5,
           'tstart': 127020624.552,
           'tstop': 3187296066.184,
           'vid_board': 0,
-          'dither': None}
+          'dither': 'NULL'}
 
 
 def decode_power(mnem):
@@ -355,17 +357,22 @@ def log_mismatch(mismatches, db_states, states, i_diff):
     logging.debug(Ska.Numpy.pformat(states))
 
 
-def update_states_db(states, db):
+def update_states_db(states, db, h5):
     """Make the ``db`` database cmd_states table consistent with the supplied
     ``states``.  Match ``states`` to corresponding values in cmd_states
     tables, then delete from table at the point of a mismatch (if any).
 
     :param states: input states (numpy recarray)
     :param db: Ska.DBI.DBI object
+    :param h5: HDF5 object holding commanded states table (as h5.root.data)
 
     :rtype: None
     """
     from itertools import count, izip
+
+    # If the HDF5 version does not exist then try to make it now.
+    if h5 and not hasattr(h5.root, 'data'):
+        _make_hdf5_cmd_states(db, h5)
 
     # Get existing cmd_states from the database that overlap with states
     db_states = db.fetchall("""SELECT * from cmd_states
@@ -404,23 +411,111 @@ def update_states_db(states, db):
             # index in db_states and states.
 
         # Mismatch occured at i_diff.  Drop cmd_states after
-        # db_state[i_diff].datestart
-        cmd = ("DELETE FROM cmd_states WHERE datestart >= '%s'"
-               % db_states[i_diff].datestart)
-        logging.info('udpate_states_db: ' + cmd)
-        db.execute(cmd)
+        # db_state['datesstart'][i_diff]
+        _delete_cmd_states(db_states['datestart'][i_diff], db, h5)
+
     else:
         # No cmd_states in database so just insert all new states
         i_diff = 0
 
-    # Insert new states[i_diff:] into cmd_states
-    logging.info('udpate_states_db: inserting states[%d:%d] to cmd_states' %
-                  (i_diff, len(states)))
+    _insert_cmd_states(states, i_diff, db, h5)
+
+
+def _delete_cmd_states(datestart, db, h5):
+    """Delete cmd_states table entries that have datestart greater than or
+    equal to the supplied ``datestart`` arg.  Do this for the SQL database
+    handle ``db`` and the HDF5 object handle ``h5``.
+    """
+    # Delete rows from HDF5 table.  It can happen (mostly during testing) that
+    # the h5 file doesn't yet have the data attribute yet, meaning it hasn't
+    # been initialized with cmd_states data.
+    if h5 and hasattr(h5.root, 'data'):
+        h5d = h5.root.data
+        idxs = h5d.getWhereList("datestart >= '{}'".format(datestart))
+
+        # Be paranoid and do a couple of consistency checks here because we
+        # are always deleting from a row index to the end of the table.
+        # PyTables/HDF5 is inefficient otherwise.
+        idxs = np.sort(idxs)
+        order_ok = True
+        if len(idxs) == 0:
+            raise ValueError('Expected to delete HDF5 cmd_states after {} '
+                             'but none matched'.format(datestart))
+        if idxs[-1] != h5d.nrows - 1:
+            order_ok = False
+        if len(idxs) > 1:
+            idxs = np.sort(idxs)
+            if np.any(idxs[1:] - idxs[:-1] != 1):
+                order_ok = False
+        if not order_ok:
+            # If there is an inconsistency stop right now and also don't
+            # touch the database table.
+            raise ValueError('ERROR: HDF5 cmd_states table is not ordered '
+                             'by datestart after {}'.format(datestart))
+
+        logging.info('udpate_states_db: '
+                     'removed HDF5 cmd_states rows from {} to {}'
+                     .format(idxs[0], h5d.nrows - 1))
+        h5d.removeRows(idxs[0], h5d.nrows)
+
+    # Delete rows from database table
+    cmd = ("DELETE FROM cmd_states WHERE datestart >= '{}'"
+           .format(datestart))
+    logging.info('udpate_states_db: ' + cmd)
+    db.execute(cmd)
+
+
+def _insert_cmd_states(states, i_diff, db, h5):
+    """Insert new ``states[idiff:]`` into ``db`` and ``h5d``.
+    """
+    # As for _delete_cmd_states do the h5d insert first so if something
+    # goes wrong then it is more likely the two tables will remain
+    # consistent.
+    if h5 and hasattr(h5.root, 'data'):
+        h5d = h5.root.data
+        logging.info('udpate_states_db: '
+                     'inserting states[{}:{}] to HDF5 cmd_states'
+                     .format(i_diff, len(states)))
+        # Create new struct array from states which is in the column
+        # order required to append to h5d.
+        sdiff = states[i_diff:]
+        rows = np.empty(len(sdiff), dtype=h5d.dtype)
+        for name in sdiff.dtype.names:
+            rows[name][:] = sdiff[name]
+        h5d.append(rows)
+        h5d.flush()
+
+    logging.info('udpate_states_db: '
+                 'inserting states[{}:{}] to database cmd_states'
+                 .format(i_diff, len(states)))
+
     for state in states[i_diff:]:
         # Need commit=True for sybase -- very large inserts will fail
         db.insert(dict((x, state[x]) for x in state.dtype.names), 'cmd_states',
                   commit=True)
     db.commit()
+
+    if h5 and not hasattr(h5.root, 'data'):
+        _make_hdf5_cmd_states(db, h5)
+
+
+def _make_hdf5_cmd_states(db, h5):
+    """Make a new HDF5 command states table in ``h5`` from the existing
+    database version.
+    """
+    # This takes a little while...
+    logging.info('Reading cmd_states table from sybase, stand by ..')
+    rows = db.fetchall('select * from cmd_states')
+    if len(rows) == 0:
+        # Need some initial data in SQL version so just return
+        logging.info('No values in SQL db so doing nothing')
+        return
+
+    logging.info('Creating HDF5 cmd_states table ..')
+    h5.createTable(h5.root, 'data', rows,
+                   "Cmd_states", expectedrows=5e5)
+    h5.flush()
+    logging.info('HDF5 cmd_states table successfully created')
 
 
 def get_state0(date=None, db=None, date_margin=10, datepar='datestop'):
@@ -807,7 +902,6 @@ def cmd_set(name, *args):
                 dict(cmd='ACISPKT',
                      tlmsid='XTZ0000005'))
 
-
     def scs107():
         return (dict(dur=1.025),
                 dict(cmd='SIMTRANS',
@@ -937,6 +1031,9 @@ def get_update_cmd_states_options():
     parser.add_option("--mp_dir",
                       default='/data/mpcrit1/mplogs',
                       help="MP load directory")
+    parser.add_option("--h5file",
+                      default='cmd_states.h5',
+                      help="filename for HDF5 version of cmd_states")
     parser.add_option("--datestart",
                       help="Starting date for update (default=Now-10 days)")
     parser.add_option("--loglevel",
@@ -961,6 +1058,7 @@ def update_cmd_states():
         --server=SERVER       DBI server (<filename>|sybase)
         --user=USER           database user (default=Ska.DBI default)
         --database=DATABASE   database name (default=Ska.DBI default)
+        --h5file=H5FILE       filename for HDF5 version of cmd_states
         --datestart=DATESTART
                               Starting date for update (default=Now-10 days)
         --mp_dir=DIR          MP directory. (default=/data/mpcrit1/mplogs)
@@ -980,10 +1078,19 @@ def update_cmd_states():
     try:
         db = Ska.DBI.DBI(dbi=opt.dbi, server=opt.server, user=opt.user,
                          database=opt.database, verbose=False)
+        if opt.dbi == 'sqlite':
+            db.conn.text_factory = str
     except Exception, msg:
         logging.error('ERROR: failed to connect to {0}:{1} server: {2}'
                       .format(opt.dbi, opt.server, msg))
         sys.exit(0)
+
+    if opt.h5file:
+        filters = tables.Filters(complevel=5, complib='zlib')
+        h5 = tables.openFile(opt.h5file, mode='a', filters=filters)
+    else:
+        print 'No h5 file'
+        h5 = None
 
     # Get initial state containing the specified datestart
     logging.debug('Getting initial state0')
@@ -1013,7 +1120,31 @@ def update_cmd_states():
 
     # Update cmd_states in database
     logging.debug('Updating database cmd_states table')
-    update_states_db(states, db)
+    update_states_db(states, db, h5)
+
+    if h5 and not _check_consistency(db, h5):
+        logging.error('ERROR: database and HDF5 commands '
+                      'states are inconsistent')
+
+    # Close down for good measure.
+    db.conn.close()
+    if h5:
+        h5.close()
+
+
+def _check_consistency(db, h5):
+    """Check that the cmd_states table in ``db`` has the same length and
+    final datestart.
+    """
+    h5d = h5.root.data
+    db_len = db.fetchone('select count(*) as cnt from cmd_states')['cnt']
+    h5d_len = h5d.nrows
+    db_last = db.fetchone('select datestart from cmd_states '
+                          'order by datestart desc')
+    h5d_last = h5d[-1]
+    ok = (db_len == h5d_len and
+          db_last['datestart'] == h5d_last['datestart'])
+    return ok
 
 
 def get_cmd_states_options(STATE_VALS):
