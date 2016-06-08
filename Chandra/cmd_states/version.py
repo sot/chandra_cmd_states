35f07c6b6d0854891b886e27afb806203d5f53e7
Jean Connelly <jconnelly@cfa.harvard.edu>
2016-06-08 15:25:45 -0400

    Update version to 0.9.3

--- Chandra/cmd_states/version.py
+++ Chandra/cmd_states/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '0.9.2'
+version = '0.9.3'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
