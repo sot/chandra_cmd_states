fe7f253187940b23e6b2eb657848cf738d1f4b62
Javier Gonzalez <javier.gonzalez@cfa.harvard.edu>
2020-09-15 18:58:13 -0400

    using ska-theme

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'bootstrap-ska'
+html_theme_options = {
+logotext1: 'Ska!' ,
+logotext2: 'cmd_states',
+logotext3: '',
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
